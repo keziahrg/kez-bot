75e5e7ac66004297fc44d1eedd3767362c54ea81
keziahrg <rg.keziah@gmail.com>
2023-02-15 21:14:57 +1300

    style: add bg opacity and blur

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ interface HeaderProps {
 
 export const Header = ({ children }: HeaderProps) => {
     return (
-        <header className="bg-white font-bold text-black dark:bg-black dark:text-white">
+        <header className="sticky top-0 right-0 left-0 bg-white bg-opacity-60 font-bold text-black backdrop-blur-md dark:bg-black dark:bg-opacity-60 dark:text-white">
             <div className="mx-auto flex max-w-xl items-center justify-between px-4 py-4 ">
                 {children}
             </div>
