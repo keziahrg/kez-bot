0697a2fea30c6c2e90c8818cf6897258c9e001d0
keziahrg <rg.keziah@gmail.com>
2024-08-05 12:50:29 +0100

    chore: add no index to head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,18 @@ import "./globals.css";
 import { Header } from "@/components/Header";
 import { Analytics } from "@vercel/analytics/react";
 import { Providers } from "./providers";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "KezBot",
+  description:
+    "A custom GPT-3.5-turbo chatbot that's ready to answer all your questions about me. Built with OpenAI, Vercel Postgres, PG Vector, Prisma, Next.js and Tailwind CSS 🤖",
+  robots: {
+    index: false,
+    follow: false,
+  },
+  icons: "/favicon.svg",
+};
 
 export default function RootLayout({
   children,
@@ -10,7 +22,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full" suppressHydrationWarning>
-      <head />
       <body className="relative flex h-full flex-col bg-grey-light dark:bg-grey-dark">
         <Providers>
           <Header />
